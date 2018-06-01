ec40c879a6abc00d092ba58f958a3291c410e6f8
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-06-01 16:42:46 +0200

    commit avant deploiement

--- sirh-saisie-coordonnees-bancaires/app.js
+++ sirh-saisie-coordonnees-bancaires/app.js
@@ -1,4 +1,4 @@
-// Fetch API
+// Get collaborateurs
 fetch('https://vignouds-sgp-api.herokuapp.com/api/collaborateurs')
     .then((resp) => resp.json())
     .then(data => afficher(data))
@@ -15,3 +15,18 @@ function afficher(data) {
 
     document.querySelector('#collabs').innerHTML = lignes
 }
+
+function sauvegarder() {
+  var myInit = {
+    method: 'PUT'
+  }
+  var myBody = {
+    banque: document.querySelector('#banque').value,
+    bic: document.querySelector('#bic').value,
+    iban: document.querySelector('#iban').value
+  }
+  var myRequest = new Request(myBody, myInit);
+  console.log(myBody);
+}
+
+document.getElementById('sauvegarder').addEventListener("click", sauvegarder());
